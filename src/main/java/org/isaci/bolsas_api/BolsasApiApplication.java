8716b7470a0474eebe29cfd82e7caab7b0c93536
hugokuribayashi <hkuribayashi@gmail.com>
2025-11-05 01:35:57 -0300

    Ajuste no CI/CD

--- src/main/java/org/isaci/bolsas_api/BolsasApiApplication.java
+++ src/main/java/org/isaci/bolsas_api/BolsasApiApplication.java
@@ -1,13 +1,22 @@
 package org.isaci.bolsas_api;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @SpringBootApplication
 public class BolsasApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BolsasApiApplication.class, args);
-	}
+    private static final Logger log = LoggerFactory.getLogger(BolsasApiApplication.class);
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = SpringApplication.run(BolsasApiApplication.class, args);
+        Environment env = ctx.getEnvironment();
+        log.info("🚀 Perfil ativo: {}", String.join(", ", env.getActiveProfiles()));
+    }
 
 }
